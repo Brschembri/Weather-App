b6c0e02417892cc6d62b1d0976aacfe3d00a3190
Brandon S <brschembri@gmail.com>
2020-10-29 14:15:10 +1100

    Added functionality to replace city name

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const cityName = document.getElementById('city-header');
 const submitBtn = document.getElementById("search-bar-bttn");
 
+submitBtn.addEventListener('click', submitIt);
 
 
+ function submitIt(e) {
+   cityName.textContent = ("Sydsney, Au");
+    // cityName.textContent = (submitBtn.textContent);
+
+
+
+   e.preventDefault();
+ }
 
