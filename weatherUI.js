7137ca28e67015ebe7be73175da662811cf1d1ac
Brandon S <brschembri@gmail.com>
2020-11-02 11:45:21 +1100

    added to weatherUI + fixed styles for min+ max

--- weatherUI.js
+++ weatherUI.js
@@ -1,12 +1,19 @@
 class Data {
     constructor() {
         this.city = document.getElementById("search-bar-input");
-        this.temp = document.getElementsByClassName("weather-temp");
-        this.pic = document.getElementsByClassName("weather-pic");
+        this.tempMaxC = document.getElementsByClassName("weather-temp");
+        this.tempMinC = document.getElementsByClassName("weather-temp-min");
+        // this.pic = document.getElementsByClassName("weather-pic");
+        this.pic1 = document.getElementById("day1");
+        this.pic1 = document.getElementById("day2");
+        this.pic1 = document.getElementById("day3");
         this.day = document.getElementsByClassName("weather-day");
     }
 
     paint(weather) {
         this.city.textContent = weather.location.name;
+        this.tempMaxC = weather.forecast.forecastday.maxtemp_c;
+        this.tempMinC = weather.forecast.forecastday.mintemp_c;
+        this.pic1 = weather.forecast.condition.icon;
     }
 }
